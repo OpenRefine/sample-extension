5503a5f99ec18cad8a562e0e456ceb45b9d36201
Antonin Delpeuch <antonin@delpeuch.eu>
2024-05-26 09:44:45 +0200

    Add dependency to the main test-jar (#8)
    * Add dependency to the main test-jar
    
    * Make sample test rely on RefineTest

--- src/test/java/com/google/refine/sampleExtension/SampleUtilTest.java
+++ src/test/java/com/google/refine/sampleExtension/SampleUtilTest.java
@@ -3,12 +3,23 @@ package com.google.refine.sampleExtension;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertThrows;
 
-import org.testng.annotations.Test;
+import java.io.Serializable;
 
-public class SampleUtilTest {
+import org.testng.annotations.Test;
+import com.google.refine.RefineTest;
+import com.google.refine.model.Project;
+
+public class SampleUtilTest extends RefineTest {
     
     @Test
     public void testStringArrayLength() {
+        // you can use the same sort of test utilities as in OpenRefine's own test suite
+        Project project = createProject(new String[] { "first column", "second column" },
+                new Serializable[][] {
+                  { "a", "b" },
+                  { "c", 3 }
+        });
+
         String[] myArray = new String[] { "foo", "bar" };
         
         assertEquals(SampleUtil.stringArrayLength(myArray), 2);
